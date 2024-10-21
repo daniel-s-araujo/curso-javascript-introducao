026c8cdc25885f0e4dbb470ff6e2a683b615983f
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-21 19:30:33 -0300

    Atribuição e imutabilidade

---
+++ Projeto/5-atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log('Trabalhando com atribuição de variáveis');
+
+const idade = 29;
+const primeiroNome = 'Ricardo';
+const sobrenome = 'Bugan';
+
+console.log(primeiroNome + ' ' + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+let contador = 0;
+contatodr = contador + 1;
