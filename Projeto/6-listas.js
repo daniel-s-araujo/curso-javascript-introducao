f534b732a815a8f66aebfb23634a6afd55213ae3
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-22 19:37:05 -0300

    Removendo elementos

--- Projeto/6-listas.js
+++ Projeto/6-listas.js
@@ -4,3 +4,7 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 listaDeDestinos.push(`Curitiba`);
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1, 1);
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[1]);
