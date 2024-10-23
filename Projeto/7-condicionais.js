8765367529a8aa970ce3b86cfc214a8b65f2297a
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-23 13:45:06 -0300

    Operadores Lógicos encadeados

--- Projeto/7-condicionais.js
+++ Projeto/7-condicionais.js
@@ -2,20 +2,26 @@ console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
-const idadeComprador = 15;
+const idadeComprador = 18;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-  console.log("Comprador maior de idade");
-  listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhada) {
-  console.log("Comprador está acompanhado");
+if (idadeComprador >= 18 || estaAcompanhada) {
+  console.log("Boa viagem");
   listaDeDestinos.splice(1, 1);
 } else {
   console.log("Não é maior de idade e não posso vender");
+}
+
+console.log(`Embarque: \n\n`);
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+  console.log("Boa viagem");
+} else {
+  console.log("Você não pode embarcar");
 }
 
 console.log(listaDeDestinos);
