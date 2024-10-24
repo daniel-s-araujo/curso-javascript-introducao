2c10b1403d3143f030989fa668b18420e290c972
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-24 19:21:56 -0300

    While

---
+++ Projeto/8-loops.js
@@ -0,0 +1,24 @@
+console.log(`\n Trabalhando com loops`);
+
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+const idadeComprador = 18;
+const estaAcompanhada = true;
+let temPassagemComprada = false;
+const destino = "São Paulo";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada;
+
+let contador = 0;
+
+while (contador < 3) {
+  if (listaDeDestinos[contador] == destino) {
+    console.log("Destino existe");
+  } else {
+    console.log("Destino não existe");
+  }
+  contador += 1;
+}
