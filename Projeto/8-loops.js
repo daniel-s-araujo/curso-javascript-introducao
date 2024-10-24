d6cd16114a3a0fed462f463c6cd75e31e9ba0c7e
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-24 19:32:15 -0300

    Break e Debbuger

--- Projeto/8-loops.js
+++ Projeto/8-loops.js
@@ -5,7 +5,7 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 const idadeComprador = 18;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "São Paulo";
+const destino = "Salvador";
 
 console.log("\n Destinos possíveis:");
 console.log(listaDeDestinos);
@@ -13,12 +13,15 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
 let contador = 0;
+let destinoExiste = false;
 
 while (contador < 3) {
   if (listaDeDestinos[contador] == destino) {
-    console.log("Destino existe");
-  } else {
-    console.log("Destino não existe");
+    destinoExiste = true;
+    break;
   }
+
   contador += 1;
 }
+
+console.log("Destino existe: ", destinoExiste);
